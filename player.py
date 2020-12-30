6498f6546099384cd70a0f96dd55c5aebfef75cd
IACEI <39549576+IACEI@users.noreply.github.com>
2020-12-30 18:23:07 +0200

    Update player.py

--- player.py
+++ player.py
@@ -50,7 +50,6 @@ class HumanPlayer(Player):
                 valid_square = True 
 
             except ValueError:
-
                 print('Invalid square, Try again')
 
         return val   
@@ -60,11 +59,13 @@ class GeniusComputerPlayer(Player):
         super().__init__(letter)
     
     def get_move(self,game):
+        
         if len(game.available_moves()) == 9:
             square = random.choice(game.available_moves())
-        else:
             
+        else:
             square = self.minimax(game,self.letter)['position']
+            
         return square
 
     def minimax(self, state,player):
@@ -72,33 +73,25 @@ class GeniusComputerPlayer(Player):
         other_player = 'O' if player == 'X' else 'X'
 
        
-        if state.current_winner == other_player:
-           
+        if state.current_winner == other_player:   
             return {'position':None,
                     'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (
-                        state.num_empty_squares() + 1)
-            }
+                        state.num_empty_squares() + 1)}
+        
         elif not state.empty_squares():
             return {'position':None , 'score':0}
 
        
         if player == max_player:
             best = {'position':None,'score': -math.inf} 
+            
         else:
             best = {'position':None,'score': math.inf}  
         
         for possible_move in state.available_moves():
-
             
-
-            state.make_move(possible_move,player)
-
-            
-
-            sim_score = self.minimax(state,other_player) 
-
-            
-
+            state.make_move(possible_move,player)         
+            sim_score = self.minimax(state,other_player)    
             state.board[possible_move] = ' '
             state.current_winner = None
             sim_score['position'] = possible_move
